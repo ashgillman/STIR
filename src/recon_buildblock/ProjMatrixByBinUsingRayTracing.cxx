ae84c3366b7b9c2d23c15b54bd85bf9e9f038783
Ashley Gillman <gillmanash@gmail.com>
2018-09-03 12:05:39 +0100

    [REF] Use m_in_mm instead of t_in_mm in ProjMatrixByBinUsingRayTracing

--- src/recon_buildblock/ProjMatrixByBinUsingRayTracing.cxx
+++ src/recon_buildblock/ProjMatrixByBinUsingRayTracing.cxx
@@ -436,7 +436,7 @@ sign(const T& t)
 static void
 ray_trace_one_lor(ProjMatrixElemsForOneBin& lor,
                   const float s_in_mm,
-                  const float t_in_mm,
+                  const float m_in_mm,
                   const float cphi,
                   const float sphi,
                   const float costheta,
@@ -512,10 +512,10 @@ ray_trace_one_lor(ProjMatrixElemsForOneBin& lor,
 
     start_point.x() = (s_in_mm * cphi + max_a * sphi) / voxel_size.x();
     start_point.y() = (s_in_mm * sphi - max_a * cphi) / voxel_size.y();
-    start_point.z() = (t_in_mm / costheta + offset_in_z - max_a * tantheta) / voxel_size.z();
+    start_point.z() = (m_in_mm + offset_in_z - max_a * tantheta) / voxel_size.z();
     stop_point.x() = (s_in_mm * cphi + min_a * sphi) / voxel_size.x();
     stop_point.y() = (s_in_mm * sphi - min_a * cphi) / voxel_size.y();
-    stop_point.z() = (t_in_mm / costheta + offset_in_z - min_a * tantheta) / voxel_size.z();
+    stop_point.z() = (m_in_mm + offset_in_z - min_a * tantheta) / voxel_size.z();
 
 #if 0
     // KT 18/05/2005 this is no longer necessary
@@ -657,7 +657,7 @@ ProjMatrixByBinUsingRayTracing::calculate_proj_matrix_elems_for_one_bin(ProjMatr
 
   const float tantheta = proj_data_info_sptr->get_tantheta(bin);
   const float costheta = 1 / sqrt(1 + square(tantheta));
-  const float t_in_mm = proj_data_info_sptr->get_t(bin);
+  const float m_in_mm = proj_data_info_sptr->get_m(bin);
 
   const float sampling_distance_of_adjacent_LORs_z = proj_data_info_sptr->get_sampling_in_t(bin) / costheta;
 
@@ -712,10 +712,10 @@ ProjMatrixByBinUsingRayTracing::calculate_proj_matrix_elems_for_one_bin(ProjMatr
     {
       // make sure we don't ray-trace exactly between 2 planes
       // z-coordinate (in voxel units) will be
-      //  (t_in_mm+offset_in_z)/voxel_size.z();
+      //  (m_in_mm+offset_in_z)/voxel_size.z();
       // if so, we ray trace first to the voxels at smaller z, but will add the
       // other plane later (in add_adjacent_z)
-      if (fabs(modulo((t_in_mm + offset_in_z) / voxel_size.z(), 1.F) - .5) < .001)
+      if (fabs(modulo((m_in_mm + offset_in_z) / voxel_size.z(), 1.F) - .5) < .001)
         offset_in_z -= .1F * voxel_size.z();
     }
 
@@ -738,7 +738,7 @@ ProjMatrixByBinUsingRayTracing::calculate_proj_matrix_elems_for_one_bin(ProjMatr
     {
       ray_trace_one_lor(lor,
                         s_in_mm,
-                        t_in_mm,
+                        m_in_mm,
                         cphi,
                         sphi,
                         costheta,
@@ -763,7 +763,7 @@ ProjMatrixByBinUsingRayTracing::calculate_proj_matrix_elems_for_one_bin(ProjMatr
           ray_traced_lor.erase();
           ray_trace_one_lor(ray_traced_lor,
                             current_s_in_mm,
-                            t_in_mm,
+                            m_in_mm,
                             cphi,
                             sphi,
                             costheta,
@@ -785,8 +785,8 @@ ProjMatrixByBinUsingRayTracing::calculate_proj_matrix_elems_for_one_bin(ProjMatr
         {
           const float z_of_first_voxel
               = lor.begin()->coord1() + origin.z() / voxel_size.z() - (max_index.z() + min_index.z()) / 2.F;
-          const float left_edge_of_TOR = (t_in_mm - sampling_distance_of_adjacent_LORs_z / 2) / voxel_size.z();
-          const float right_edge_of_TOR = (t_in_mm + sampling_distance_of_adjacent_LORs_z / 2) / voxel_size.z();
+          const float left_edge_of_TOR = (m_in_mm - sampling_distance_of_adjacent_LORs_z / 2) / voxel_size.z();
+          const float right_edge_of_TOR = (m_in_mm + sampling_distance_of_adjacent_LORs_z / 2) / voxel_size.z();
 
           add_adjacent_z(lor, z_of_first_voxel - left_edge_of_TOR, right_edge_of_TOR - left_edge_of_TOR);
         }
